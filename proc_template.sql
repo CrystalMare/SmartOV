50e2c25e3bdc0c884a6671f9b9c1fedfe04438d9
Sven Olderaan <admin@heaven-craft.net>
2016-05-12 16:18:15 +0200

    Added DDL Files

---
+++ proc_template.sql
@@ -0,0 +1,40 @@
+USE EXAMPLEDATABASE -- EXAMPLE DATABASENAME
+GO
+
+IF OBJECT_ID('SaveTranExample', 'P') IS NOT NULL -- EXAMPLE PROCEDURE NAME
+  DROP PROCEDURE SaveTranExample
+GO
+
+-- CREATE STORED PROCEDURE
+CREATE PROCEDURE SaveTranExample -- EXAMPLE NAME
+    @InputCandidateID INT -- EXAMPLE PARAMETERS
+AS
+  DECLARE @TranCounter INT;
+  SET @TranCounter = @@TRANCOUNT;
+  IF @TranCounter > 0
+    SAVE TRANSACTION ProcedureSave;
+  ELSE
+    BEGIN TRANSACTION;
+  BEGIN TRY
+
+  -- ADD CODE
+
+  IF @TranCounter = 0
+    COMMIT TRANSACTION;
+
+  END TRY
+  BEGIN CATCH
+    IF @TranCounter = 0
+      ROLLBACK TRANSACTION;
+    ELSE
+      IF XACT_STATE() <> -1
+        ROLLBACK TRANSACTION ProcedureSave;
+
+    DECLARE @ErrorMessage NVARCHAR(4000);
+    DECLARE @ErrorSeverity INT;
+    DECLARE @ErrorState INT;
+    SELECT @ErrorMessage = ERROR_MESSAGE();
+    SELECT @ErrorSeverity = ERROR_SEVERITY();
+    SELECT @ErrorState = ERROR_STATE();
+    RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+  END CATCH
