9208192f8b7a5c8f0a665507868142c2fa1e8895
Frank Blom <frankblom93@gmail.com>
2016-05-25 15:16:30 +0200

    getproducts van kaart aangemaakt. Checked of kaart bestaat en geeft een tabel met reisproducten terug

---
+++ sql/stored_procedures/proc_get_products.sql
@@ -0,0 +1,47 @@
+USE smartov -- EXAMPLE DATABASENAME
+GO
+
+EXECUTE sp_addmessage 56080, 16, 'Kaart is niet gevonden!';
+
+IF OBJECT_ID('PROC_GET_PRODUCTS', 'P') IS NOT NULL -- EXAMPLE PROCEDURE NAME
+  DROP PROCEDURE PROC_GET_PRODUCTS
+GO
+
+-- CREATE STORED PROCEDURE
+CREATE PROCEDURE PROC_GET_PRODUCTS -- EXAMPLE NAME
+    @kaartid UNIQUEIDENTIFIER
+AS
+  DECLARE @TranCounter INT;
+  SET @TranCounter = @@TRANCOUNT;
+  IF @TranCounter > 0
+    SAVE TRANSACTION ProcedureSave;
+  ELSE
+    BEGIN TRANSACTION;
+  BEGIN TRY
+
+  IF NOT EXISTS(SELECT 1 FROM dbo.KAART WHERE KAARTID = @kaartid)
+      RAISERROR (56080, 16, 1);
+
+  SELECT *
+  FROM dbo.REISPRODUCT
+  WHERE KAARTID = @kaartid
+
+  IF @TranCounter = 0
+    COMMIT TRANSACTION;
+
+  END TRY
+  BEGIN CATCH
+    IF @TranCounter = 0
+      ROLLBACK TRANSACTION;
+    ELSE
+      IF XACT_STATE() <> -1
+        ROLLBACK TRANSACTION ProcedureSave;
+
+    DECLARE @ErrorMessage NVARCHAR(4000);
+    DECLARE @ErrorSeverity INT;
+    DECLARE @ErrorState INT;
+    SELECT @ErrorMessage = ERROR_MESSAGE();
+    SELECT @ErrorSeverity = ERROR_SEVERITY();
+    SELECT @ErrorState = ERROR_STATE();
+    RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+  END CATCH
