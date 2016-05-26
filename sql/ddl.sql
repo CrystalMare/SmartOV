a80b1a87ba79870de93e719f3aca1e95e83a3242
Frank Blom <frankblom93@gmail.com>
2016-05-26 11:43:40 +0200

    ddl scripts vervangen met ddl.sql. Deze file wordt vanaf nu geupdated.

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      Microsoft SQL Server 2012                    */
-/* Created on:     25-5-2016 10:29:33                           */
+/* Created on:     26-5-2016 11:41:51                           */
 /*==============================================================*/
 
 
@@ -20,9 +20,23 @@ go
 
 if exists (select 1
    from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('EENMALIGE_REISPRODUCT') and o.name = 'FK_EENMALIG_BESTAAT_U_REISPROD')
+alter table EENMALIGE_REISPRODUCT
+   drop constraint FK_EENMALIG_BESTAAT_U_REISPROD
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
    where r.fkeyid = object_id('KAART') and o.name = 'FK_KAART_GEKOPPELD_ACCOUNT')
 alter table KAART
    drop constraint FK_KAART_GEKOPPELD_ACCOUNT
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('KORTINGSREISPRODUCT') and o.name = 'FK_KORTINGS_BESTAAT_U_REISPROD')
+alter table KORTINGSREISPRODUCT
+   drop constraint FK_KORTINGS_BESTAAT_U_REISPROD
 go
 
 if exists (select 1
@@ -39,78 +53,236 @@ alter table PERSOONLIJKE_KAART
    drop constraint FK_PERSOONL_IS_KAARTH_PERSOON
 go
 
-alter table ACCOUNT
-   drop constraint PK_ACCOUNT
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('PRODUCT_OP_KAART') and o.name = 'FK_PRODUCT__GEKOPPELD_KAART')
+alter table PRODUCT_OP_KAART
+   drop constraint FK_PRODUCT__GEKOPPELD_KAART
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('PRODUCT_OP_KAART') and o.name = 'FK_PRODUCT__GEKOPPELD_REISPROD')
+alter table PRODUCT_OP_KAART
+   drop constraint FK_PRODUCT__GEKOPPELD_REISPROD
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('REGIOPRODUCT') and o.name = 'FK_REGIOPRO_IS_GELDIG_REGIO')
+alter table REGIOPRODUCT
+   drop constraint FK_REGIOPRO_IS_GELDIG_REGIO
+go
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('REGIOPRODUCT') and o.name = 'FK_REGIOPRO_SUBTYPE_V_KORTINGS')
+alter table REGIOPRODUCT
+   drop constraint FK_REGIOPRO_SUBTYPE_V_KORTINGS
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('ACCOUNT')
+            and   name  = 'FK_IS_SALDOBEHEERDER_VAN'
+            and   indid > 0
+            and   indid < 255)
+   drop index ACCOUNT.FK_IS_SALDOBEHEERDER_VAN
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('tmp_ACCOUNT')
+           where  id = object_id('ACCOUNT')
             and   type = 'U')
-   drop table tmp_ACCOUNT
-go
-
-execute sp_rename ACCOUNT, tmp_ACCOUNT
-go
-
-alter table ANONIEME_KAART
-   drop constraint PK_ANONIEME_KAART
+   drop table ACCOUNT
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('tmp_ANONIEME_KAART')
+           where  id = object_id('ANONIEME_KAART')
             and   type = 'U')
-   drop table tmp_ANONIEME_KAART
-go
-
-execute sp_rename ANONIEME_KAART, tmp_ANONIEME_KAART
-go
-
-alter table KAART
-   drop constraint AK_IDENTIFIER_1_KAART
-go
-
-alter table KAART
-   drop constraint PK_KAART
+   drop table ANONIEME_KAART
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('tmp_KAART')
+           where  id = object_id('EENMALIGE_REISPRODUCT')
             and   type = 'U')
-   drop table tmp_KAART
+   drop table EENMALIGE_REISPRODUCT
 go
 
-execute sp_rename KAART, tmp_KAART
-go
-
-alter table PERSOON
-   drop constraint PK_PERSOON
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('KAART')
+            and   name  = 'FK_GEKOPPELD_AAN'
+            and   indid > 0
+            and   indid < 255)
+   drop index KAART.FK_GEKOPPELD_AAN
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('tmp_PERSOON')
+           where  id = object_id('KAART')
             and   type = 'U')
-   drop table tmp_PERSOON
-go
-
-execute sp_rename PERSOON, tmp_PERSOON
-go
-
-alter table PERSOONLIJKE_KAART
-   drop constraint PK_PERSOONLIJKE_KAART
+   drop table KAART
 go
 
 if exists (select 1
             from  sysobjects
-           where  id = object_id('tmp_PERSOONLIJKE_KAART')
+           where  id = object_id('KORTINGSREISPRODUCT')
             and   type = 'U')
-   drop table tmp_PERSOONLIJKE_KAART
+   drop table KORTINGSREISPRODUCT
 go
 
-execute sp_rename PERSOONLIJKE_KAART, tmp_PERSOONLIJKE_KAART
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('PERSOON')
+            and   type = 'U')
+   drop table PERSOON
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('PERSOONLIJKE_KAART')
+            and   name  = 'FK_IS_KAARTHOUDER_VAN'
+            and   indid > 0
+            and   indid < 255)
+   drop index PERSOONLIJKE_KAART.FK_IS_KAARTHOUDER_VAN
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('PERSOONLIJKE_KAART')
+            and   type = 'U')
+   drop table PERSOONLIJKE_KAART
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('PRODUCT_OP_KAART')
+            and   name  = 'FK_GEKOPPELD_AAN3'
+            and   indid > 0
+            and   indid < 255)
+   drop index PRODUCT_OP_KAART.FK_GEKOPPELD_AAN3
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('PRODUCT_OP_KAART')
+            and   name  = 'FK_GEKOPPELD_AAN2'
+            and   indid > 0
+            and   indid < 255)
+   drop index PRODUCT_OP_KAART.FK_GEKOPPELD_AAN2
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('PRODUCT_OP_KAART')
+            and   type = 'U')
+   drop table PRODUCT_OP_KAART
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('REGIO')
+            and   type = 'U')
+   drop table REGIO
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('REGIOPRODUCT')
+            and   name  = 'FK_IS_GELDIG_IN'
+            and   indid > 0
+            and   indid < 255)
+   drop index REGIOPRODUCT.FK_IS_GELDIG_IN
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('REGIOPRODUCT')
+            and   type = 'U')
+   drop table REGIOPRODUCT
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('REISPRODUCT')
+            and   type = 'U')
+   drop table REISPRODUCT
+go
+
+if exists(select 1 from systypes where name='BANKREKENING')
+   drop type BANKREKENING
+go
+
+if exists(select 1 from systypes where name='DAGEN')
+   drop type DAGEN
+go
+
+if exists(select 1 from systypes where name='DATUM')
+   drop type DATUM
+go
+
+if exists(select 1 from systypes where name='EMAILADRES')
+   drop type EMAILADRES
+go
+
+if exists(select 1 from systypes where name='GELD')
+   drop type GELD
+go
+
+if exists(select 1 from systypes where name='HUISNUMMER')
+   drop type HUISNUMMER
+go
+
+if exists(select 1 from systypes where name='KAARTNAAM')
+   drop type KAARTNAAM
+go
+
+if exists(select 1 from systypes where name='KAARTNUMMER')
+   drop type KAARTNUMMER
+go
+
+if exists(select 1 from systypes where name='NAAM')
+   drop type NAAM
+go
+
+if exists(select 1 from systypes where name='PERCENTAGE')
+   drop type PERCENTAGE
+go
+
+if exists(select 1 from systypes where name='POSTCODE')
+   drop type POSTCODE
+go
+
+if exists(select 1 from systypes where name='SURROGATEKEY')
+   drop type SURROGATEKEY
+go
+
+if exists(select 1 from systypes where name='TELEFOONNUMMER')
+   drop type TELEFOONNUMMER
+go
+
+/*==============================================================*/
+/* Domain: BANKREKENING                                         */
+/*==============================================================*/
+create type BANKREKENING
+   from varchar(34)
+go
+
+/*==============================================================*/
+/* Domain: DAGEN                                                */
+/*==============================================================*/
+create type DAGEN
+   from int
+go
+
+/*==============================================================*/
+/* Domain: DATUM                                                */
+/*==============================================================*/
+create type DATUM
+   from datetime
 go
 
 /*==============================================================*/
@@ -118,6 +290,34 @@ go
 /*==============================================================*/
 create type EMAILADRES
    from varchar(255)
+go
+
+/*==============================================================*/
+/* Domain: GELD                                                 */
+/*==============================================================*/
+create type GELD
+   from money
+go
+
+/*==============================================================*/
+/* Domain: HUISNUMMER                                           */
+/*==============================================================*/
+create type HUISNUMMER
+   from varchar(5)
+go
+
+/*==============================================================*/
+/* Domain: KAARTNAAM                                            */
+/*==============================================================*/
+create type KAARTNAAM
+   from varchar(26) not null
+go
+
+/*==============================================================*/
+/* Domain: KAARTNUMMER                                          */
+/*==============================================================*/
+create type KAARTNUMMER
+   from char(16)
 go
 
 /*==============================================================*/
@@ -135,6 +335,27 @@ create type PERCENTAGE
 go
 
 /*==============================================================*/
+/* Domain: POSTCODE                                             */
+/*==============================================================*/
+create type POSTCODE
+   from char(6)
+go
+
+/*==============================================================*/
+/* Domain: SURROGATEKEY                                         */
+/*==============================================================*/
+create type SURROGATEKEY
+   from uniqueidentifier
+go
+
+/*==============================================================*/
+/* Domain: TELEFOONNUMMER                                       */
+/*==============================================================*/
+create type TELEFOONNUMMER
+   from varchar(15)
+go
+
+/*==============================================================*/
 /* Table: ACCOUNT                                               */
 /*==============================================================*/
 create table ACCOUNT (
@@ -144,11 +365,6 @@ create table ACCOUNT (
    REKENINGNUMMER       BANKREKENING         null,
    constraint PK_ACCOUNT primary key (ACCOUNTID)
 )
-go
-
-insert into ACCOUNT (ACCOUNTID, PERSOONID, SALDO, REKENINGNUMMER)
-select ACCOUNTID, PERSOONID, SALDO, REKENINGNUMMER
-from tmp_ACCOUNT
 go
 
 /*==============================================================*/
@@ -168,11 +384,6 @@ create table ANONIEME_KAART (
    KAARTID              SURROGATEKEY         not null,
    constraint PK_ANONIEME_KAART primary key (KAARTID)
 )
-go
-
-insert into ANONIEME_KAART (KAARTID)
-select KAARTID
-from tmp_ANONIEME_KAART
 go
 
 /*==============================================================*/
@@ -198,11 +409,6 @@ create table KAART (
    constraint PK_KAART primary key (KAARTID),
    constraint AK_IDENTIFIER_1_KAART unique (KAARTNUMMER)
 )
-go
-
-insert into KAART (KAARTID, ACCOUNTID, KAARTNUMMER, KAARTNAAM, VERVALDATUM, KOPPELDATUM)
-select KAARTID, ACCOUNTID, KAARTNUMMER, KAARTNAAM, VERVALDATUM, KOPPELDATUM
-from tmp_KAART
 go
 
 /*==============================================================*/
@@ -240,11 +446,6 @@ create table PERSOON (
 )
 go
 
-insert into PERSOON (PERSOONID, NAAM, POSTCODE, HUISNUMMER, GEBOORTEDATUM, TELEFOONNUMMER, E_MAILADRES)
-select PERSOONID, NAAM, POSTCODE, HUISNUMMER, GEBOORTEDATUM, TELEFOONNUMMER, E_MAILADRES
-from tmp_PERSOON
-go
-
 /*==============================================================*/
 /* Table: PERSOONLIJKE_KAART                                    */
 /*==============================================================*/
@@ -255,11 +456,6 @@ create table PERSOONLIJKE_KAART (
 )
 go
 
-insert into PERSOONLIJKE_KAART (KAARTID, PERSOONID)
-select KAARTID, PERSOONID
-from tmp_PERSOONLIJKE_KAART
-go
-
 /*==============================================================*/
 /* Index: FK_IS_KAARTHOUDER_VAN                                 */
 /*==============================================================*/
@@ -268,6 +464,37 @@ go
 
 
 create nonclustered index FK_IS_KAARTHOUDER_VAN on PERSOONLIJKE_KAART (PERSOONID ASC)
+go
+
+/*==============================================================*/
+/* Table: PRODUCT_OP_KAART                                      */
+/*==============================================================*/
+create table PRODUCT_OP_KAART (
+   KAARTID              SURROGATEKEY         not null,
+   REISPRODUCTID        SURROGATEKEY         not null,
+   KOPPELDATUM          DATUM                not null,
+   constraint PK_PRODUCT_OP_KAART primary key nonclustered (KAARTID, REISPRODUCTID)
+)
+go
+
+/*==============================================================*/
+/* Index: FK_GEKOPPELD_AAN2                                     */
+/*==============================================================*/
+
+
+
+
+create nonclustered index FK_GEKOPPELD_AAN2 on PRODUCT_OP_KAART (KAARTID ASC)
+go
+
+/*==============================================================*/
+/* Index: FK_GEKOPPELD_AAN3                                     */
+/*==============================================================*/
+
+
+
+
+create nonclustered index FK_GEKOPPELD_AAN3 on PRODUCT_OP_KAART (REISPRODUCTID ASC)
 go
 
 /*==============================================================*/
@@ -305,22 +532,10 @@ go
 /*==============================================================*/
 create table REISPRODUCT (
    REISPRODUCTID        SURROGATEKEY         not null,
-   KAARTID              SURROGATEKEY         not null,
    NAAM                 NAAM                 not null,
-   KOPPELDATUM          DATUM                not null,
-   VERVALDATUM          DATUM                not null,
+   GELDIGHEID           DAGEN                not null,
    constraint PK_REISPRODUCT primary key (REISPRODUCTID)
 )
-go
-
-/*==============================================================*/
-/* Index: FK_STAAT_OP                                           */
-/*==============================================================*/
-
-
-
-
-create nonclustered index FK_STAAT_OP on REISPRODUCT (KAARTID ASC)
 go
 
 alter table ACCOUNT
@@ -358,6 +573,16 @@ alter table PERSOONLIJKE_KAART
       references PERSOON (PERSOONID)
 go
 
+alter table PRODUCT_OP_KAART
+   add constraint FK_PRODUCT__GEKOPPELD_KAART foreign key (KAARTID)
+      references KAART (KAARTID)
+go
+
+alter table PRODUCT_OP_KAART
+   add constraint FK_PRODUCT__GEKOPPELD_REISPROD foreign key (REISPRODUCTID)
+      references REISPRODUCT (REISPRODUCTID)
+go
+
 alter table REGIOPRODUCT
    add constraint FK_REGIOPRO_IS_GELDIG_REGIO foreign key (REGIOID)
       references REGIO (REGIOID)
@@ -366,10 +591,5 @@ go
 alter table REGIOPRODUCT
    add constraint FK_REGIOPRO_SUBTYPE_V_KORTINGS foreign key (REISPRODUCTID)
       references KORTINGSREISPRODUCT (REISPRODUCTID)
-go
-
-alter table REISPRODUCT
-   add constraint FK_REISPROD_STAAT_OP_KAART foreign key (KAARTID)
-      references KAART (KAARTID)
 go
 
