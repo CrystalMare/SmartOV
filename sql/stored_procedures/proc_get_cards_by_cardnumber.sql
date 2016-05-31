d0424be6acdb2f030550af56901bbda88581e62d
Steven van Tuil <SQD.vantuil@student.han.nl>
2016-05-31 14:31:38 +0200

    Proc_get_cards_by_cardnumber gemaakt en op de server geüpload

---
+++ sql/stored_procedures/proc_get_cards_by_cardnumber.sql
@@ -0,0 +1,44 @@
+IF OBJECT_ID('PROC_GET_CARDS_BY_CARDNUMBER', 'P') IS NOT NULL -- EXAMPLE PROCEDURE NAME
+  DROP PROCEDURE PROC_GET_CARDS_BY_CARDNUMBER
+GO
+
+-- CREATE STORED PROCEDURE
+CREATE PROCEDURE PROC_GET_CARDS_BY_CARDNUMBER -- EXAMPLE NAME
+    @cardnumber int -- EXAMPLE PARAMETERS
+AS
+  DECLARE @TranCounter INT;
+  SET @TranCounter = @@TRANCOUNT;
+  IF @TranCounter > 0
+    SAVE TRANSACTION ProcedureSave;
+  ELSE
+    BEGIN TRANSACTION;
+  BEGIN TRY
+
+--   IF NOT EXISTS (SELECT 1 FROM dbo.ACCOUNT WHERE @cardnumber = ACCOUNTID)
+--     RAISERROR(56050, 16, 1);
+
+  SELECT *
+  FROM dbo.KAART
+  WHERE @cardnumber = KAARTNUMMER
+
+  IF @TranCounter = 0
+    COMMIT TRANSACTION;
+
+  END TRY
+  BEGIN CATCH
+    IF @TranCounter = 0
+      ROLLBACK TRANSACTION;
+    ELSE
+      IF XACT_STATE() <> -1
+        ROLLBACK TRANSACTION ProcedureSave;
+
+    DECLARE @ErrorMessage NVARCHAR(4000);
+    DECLARE @ErrorSeverity INT;
+    DECLARE @ErrorState INT;
+    SELECT @ErrorMessage = ERROR_MESSAGE();
+    SELECT @ErrorSeverity = ERROR_SEVERITY();
+    SELECT @ErrorState = ERROR_STATE();
+    RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+  END CATCH
+--
+-- EXECUTE sp_addmessage 56050, 16, 'Account bestaat niet', @replace = REPLACE;
