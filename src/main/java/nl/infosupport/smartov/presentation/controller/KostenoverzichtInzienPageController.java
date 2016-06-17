f71bf2d6440a3f0ed5d92891eb178b4de0171f34
Heda <H.khanpour@student.han.nl>
2016-06-17 11:14:55 +0200

    kosten inzien

--- src/main/java/nl/infosupport/smartov/presentation/controller/KostenoverzichtInzienPageController.java
+++ src/main/java/nl/infosupport/smartov/presentation/controller/KostenoverzichtInzienPageController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.math.BigInteger;
+import java.math.BigDecimal;
 import java.util.*;
 
 @WebServlet(urlPatterns = "/kostenoverzicht")
@@ -27,7 +27,7 @@ public class KostenoverzichtInzienPageController extends HttpServlet {
         SmartOV smartOV = new SmartOV();
         SmartOVDao dao = smartOV.getInstance(SmartOVDao.class);
 
-        BigInteger cost = null;
+        BigDecimal cost = null;
 
         UUID accountID = (UUID) session.getAttribute("accountId");
         Calendar cal = Calendar.getInstance();
